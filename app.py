f8c495af2d4711bb4b594a15a465e68f151366d1
Abdullah Naimzadeh <naimzaa8@gmail.com>
2024-01-16 19:48:49 -0500

    updating app.py and req file

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, request, jsonify
 from transformers import pipeline
 
 app = Flask(__name__)
-generator = pipeline('text-generation', model = 'gpt2') 
+generator = pipeline('text-generation', model='gpt2')  # Example using GPT-2
 
 @app.route('/gpt-action', methods=['POST'])
 def gpt_action():
-	data = request.json  # Extract JSON data from the request
-	prompt = data.get("message","")    
-	gpt_response = generator(prompt, max_length = 50, num_return_sequences=1)  # Pass the data to the function
-	return jsonify({"message": "GPT Model Response", "response": gpt_response[0]["generated_text"]})
+    data = request.json
+    prompt = data.get("message", "")
+    gpt_response = generator(prompt, max_length=50, num_return_sequences=1)
+    return jsonify({"message": "GPT Model Response", "response": gpt_response[0]["generated_text"]})
 
 if __name__ == '__main__':
     app.run(debug=True)
